3da65d8c4f2d9dfd0b418db60408f64fa6698af3
SamiMorris <110176624+SamiMorris@users.noreply.github.com>
2022-09-16 12:33:10 +0100

    Add files via upload

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className = "this_will_do">
+                    <div>
+                        <a href = "this_will_do" className = "this_will_do">
+                            Employees
+                        </a>
+                    </div>
+
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
